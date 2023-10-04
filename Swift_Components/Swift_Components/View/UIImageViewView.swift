1afe9cefa8183a830c1d2f744c1a29b3f3bd8af8
LEE SUNG HO <59243274+dangsal@users.noreply.github.com>
2023-10-04 14:04:39 +0900

    [CHORE] UIImageView 속성 추가

--- Swift_Components/Swift_Components/View/UIImageViewView.swift
+++ Swift_Components/Swift_Components/View/UIImageViewView.swift
@@ -13,11 +13,40 @@ final class UIImageViewView: UIView {
     
     // MARK: - ui component
     
+    private let imageView1: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .lightGray
+        imageView.image = UIImage(systemName: "heart")
+        imageView.highlightedImage = UIImage(systemName: "heart.fill")
+        imageView.tintColor = .red
+        imageView.isUserInteractionEnabled = true
+        return imageView
+    }()
+    private let imageView2: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .lightGray
+        imageView.tintColor = .red
+        imageView.isUserInteractionEnabled = true
+        imageView.animationImages = [
+            UIImage(systemName: "heart"),
+            UIImage(systemName: "heart.fill")
+        ].compactMap { $0  }
+        imageView.animationDuration = 2.0
+        imageView.animationRepeatCount = 2
+        return imageView
+    }()
+    // MARK: - addSymbolEffect, removeSymbolEffect, removeAllSymbolEffects, setSymbolImage
+    private let imageView3: UIImageView = {
+        let imageView = UIImageView()
+        return imageView
+    }()
+    
     // MARK: - init
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.setupLayout()
+        self.setupAction()
     }
     
     required init?(coder: NSCoder) {
@@ -27,6 +56,46 @@ final class UIImageViewView: UIView {
     // MARK: - func
     
     private func setupLayout() {
+        self.addSubview(self.imageView1)
+        self.imageView1.snp.makeConstraints {
+            $0.top.equalTo(self.safeAreaLayoutGuide.snp.top)
+            $0.leading.trailing.equalToSuperview().inset(20)
+            $0.height.equalTo(200)
+        }
+        
+        self.addSubview(self.imageView2)
+        self.imageView2.snp.makeConstraints {
+            $0.top.equalTo(self.imageView1.snp.bottom).offset(20)
+            $0.leading.trailing.equalToSuperview().inset(20)
+            $0.height.equalTo(200)
+        }
+        
+        self.addSubview(self.imageView3)
+        self.imageView3.snp.makeConstraints {
+            $0.top.equalTo(self.imageView2.snp.bottom).offset(20)
+            $0.leading.trailing.equalToSuperview().inset(20)
+            $0.height.equalTo(200)
+        }
+    }
+    
+    private func setupAction() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.didTapImageView1(_:)))
+        self.imageView1.addGestureRecognizer(tapGesture)
+        
+        let tapGesture2 = UITapGestureRecognizer(target: self, action: #selector(self.didTapImageView2(_:)))
+        self.imageView2.addGestureRecognizer(tapGesture2)
+    }
+    
+    // MARK: - selector
+    
+    @objc func didTapImageView1(_ gesture: UITapGestureRecognizer) {
+        if gesture.state == .ended {
+            self.imageView1.isHighlighted.toggle()
+        }
+    }
+    
+    @objc func didTapImageView2(_ gesture: UITapGestureRecognizer) {
+        self.imageView2.startAnimating()
     }
 }
 
